ba6d6c91eddb0ae6d7aef0d19b762a9971eb5b11
manic128 <manic128@e94a5bfb-cbc5-4b52-91e4-5eccdaea5047>
2008-01-10 06:48:44 +0000

    afs_utils main source

---
+++ srcpack/afsutils/variables.pas
@@ -0,0 +1,14 @@
+unit variables;
+
+interface
+
+uses Classes, UIntList;
+
+var
+input_list, files_infos, files_dates:TStringList;
+global_block_size:Integer;
+block_value_list:TIntList;
+
+implementation
+
+end.
