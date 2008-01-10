e81917206240c161831cfb8e12b450d679f59012
manic128 <manic128@e94a5bfb-cbc5-4b52-91e4-5eccdaea5047>
2008-01-10 20:57:30 +0000

    spr_utils delphi project

---
+++ srcpack/sprutils/spr_utils_v1.dpr
@@ -0,0 +1,15 @@
+program spr_utils_v1;
+
+uses
+  Forms,
+  Unit1 in 'Unit1.pas' {Form1},
+  variables in 'variables.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.Title := 'SPR Utils v1';
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
