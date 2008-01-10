86f913243b8102011b54f2576995b079c64e8685
manic128 <manic128@e94a5bfb-cbc5-4b52-91e4-5eccdaea5047>
2008-01-10 21:08:24 +0000

    idx_creator main source

---
+++ srcpack/idxcreator/variables.pas
@@ -0,0 +1,13 @@
+unit variables;
+
+interface
+
+uses Classes, UIntList;
+
+var
+files_infos:TStringList;
+files_offset, files_size, srf_list, srf_offset, current_files_list:TIntList;
+
+implementation
+
+end.
