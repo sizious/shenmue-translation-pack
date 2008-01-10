4faf513f382f10bcb82521b0b260167d3d2489c5
manic128 <manic128@e94a5bfb-cbc5-4b52-91e4-5eccdaea5047>
2008-01-10 06:41:31 +0000

    shen2_subs_editor main source

---
+++ srcpack/shen2_subseditor/variables.pas
@@ -0,0 +1,15 @@
+unit variables;
+
+interface
+
+uses
+  Classes, SysUtils, UIntList, USRFEntry;
+
+var srf_infos:TSRFEntry;
+srf_data:TList;
+chars_list:TStringList;
+chars_decimal_list:TIntList;
+
+implementation
+
+end.
