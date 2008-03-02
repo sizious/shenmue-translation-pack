166c492b69075124012a31b4a65bce9d0acc55e0
manic128 <manic128@e94a5bfb-cbc5-4b52-91e4-5eccdaea5047>
2008-03-02 14:23:53 +0000

    Updated to v1.1

---
+++ srcpack/shen1_subseditor/variables.pas
@@ -0,0 +1,16 @@
+unit variables;
+
+interface
+
+uses
+  Classes, SysUtils, UIntList, USRFEntry;
+
+var srf_infos:TSRFEntry;
+srf_data:TList;
+chars_list:TStringList;
+chars_decimal_list:TIntList;
+
+
+implementation
+
+end.
