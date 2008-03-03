150c4b4047bb5a1763b6f02c464bb24aabfaf13b
manic128 <43767048+gt-manic@users.noreply.github.com>
2008-03-03 01:59:12 +0000

    Updated to v4.2

---
+++ srcpack/shen2_subseditor/variables.pas
@@ -0,0 +1,15 @@
+unit variables;
+
+interface
+
+uses
+  Classes, SysUtils, UIntList, USRFEntry;
+
+var srf_infos:TSRFEntry;
+srf_data:TList;
+chars_list, sub_chars_list:TStringList;
+chars_decimal_list, sub_decimal_list:TIntList;
+
+implementation
+
+end.
