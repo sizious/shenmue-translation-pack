9cf4b12efe317bc70fa5b3b55a4f4c220f22fa58
manic128 <manic128@e94a5bfb-cbc5-4b52-91e4-5eccdaea5047>
2008-05-15 12:56:09 +0000

    Updated to v1.1

---
+++ srcpack/shen1_subseditor/shen1_subs_editor.dpr
@@ -0,0 +1,16 @@
+program shen1_subs_editor;
+
+uses
+  Forms,
+  Unit1 in 'Unit1.pas' {Form1},
+  variables in 'variables.pas',
+  USRFEntry in 'USRFEntry.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.Title := 'Shenmue I Subtitles Editor v1.1';
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
