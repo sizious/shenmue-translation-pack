c7551e78057d989c49edd76f0f4e0266a7decfd6
manic128 <manic128@e94a5bfb-cbc5-4b52-91e4-5eccdaea5047>
2008-06-20 16:37:26 +0000

    afsutils v2.0 main source

---
+++ srcpack/afsutils/src/engine/afsextract.pas
@@ -0,0 +1,89 @@
+unit afsextract;
+
+interface
+
+uses
+  Windows, SysUtils, Classes, UIntList, Forms;
+
+type
+  TAfsInfos = Record
+    FileName: TStringList;
+    FileOffset: TIntList;
+    FileSize: TIntList;
+    FileDate: TStringList;
+  end;
+
+type
+  TAfsQueue = Record
+    NameIndex: Integer;
+    OutputDir: String;
+    FileIndex: TIntList;
+  end;
+
+procedure InitializeVars;
+procedure FreeVars;
+procedure ClearVars;
+procedure ClearQueue;
+function StartAfsExtraction: Boolean;
+
+var
+  afsMain:TAfsInfos;
+  afsMainQueue: TAfsQueue;
+  afsFileName: TStringList;
+  doXmlList: Boolean;
+
+implementation
+uses UAfsExtraction;
+
+procedure InitializeVars;
+begin
+  afsMain.FileName := TStringList.Create;
+  afsMain.FileOffset := TIntList.Create;
+  afsMain.FileSize := TIntList.Create;
+  afsMain.FileDate := TStringList.Create;
+  afsMainQueue.FileIndex := TIntList.Create;
+  afsFileName := TStringList.Create;
+  doXmlList:= True;
+end;
+
+procedure FreeVars;
+begin
+  afsMain.FileName.Free;
+  afsMain.FileOffset.Free;
+  afsMain.FileSize.Free;
+  afsMain.FileDate.Free;
+  afsMainQueue.FileIndex.Free;
+  afsFileName.Free;
+end;
+
+procedure ClearVars;
+begin
+  afsMain.FileName.Clear;
+  afsMain.FileOffset.Clear;
+  afsMain.FileSize.Clear;
+  afsMain.FileDate.Clear;
+end;
+
+procedure ClearQueue;
+begin
+  afsMainQueue.NameIndex := -1;
+  afsMainQueue.OutputDir := '';
+  afsMainQueue.FileIndex.Clear;
+end;
+
+function StartAfsExtraction: Boolean;
+var
+  AfsExtractThread: TAfsExtraction;
+begin
+  //This will start the extraction thread with afsMainQueue content
+  Result := False;
+  afsExtractThread := TAfsExtraction.Create;
+
+  repeat
+    //Waiting until the thread is finished
+    Application.ProcessMessages;
+  until (afsExtractThread.ThreadTerminated);
+  Result := True;
+end;
+
+end.
