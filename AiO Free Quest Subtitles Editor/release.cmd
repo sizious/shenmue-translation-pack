88e41cf860ca59436460c25341eec649a65b549b
sizious <sizious@e94a5bfb-cbc5-4b52-91e4-5eccdaea5047>
2009-09-01 16:38:04 +0000

    Updating batch files.

---
+++ AiO Free Quest Subtitles Editor/release.cmd
@@ -0,0 +1,69 @@
+@echo off
+REM prompt $G 
+
+REM Program Release Package Maker is a utility made for making your ZIP file release of your program.
+title=Program Release Package Maker
+echo *** PROGRAM RELEASE PACKAGE MAKER ***
+echo Making AiO Free Quest Subtitles Editor release... Please wait.
+echo.
+
+REM Setting needed vars
+call ..\vars.cmd
+if "%RELEASE_BASE_DIR%"=="" goto error
+if "%SEVENZIP_BIN%"=="" goto error
+if not exist "%SEVENZIP_BIN%" goto error
+
+REM Setting version for the filename
+call version.cmd
+if "%VERSION%"=="" goto error
+goto make
+
+REM Exiting because vars.cmd not properly set
+:error
+echo ERROR: Some essential files wasn't found.
+echo Please edit the vars.cmd file on the root to set the release output directory.
+pause
+goto end
+
+REM Making release!
+:make
+if exist %UPX_BIN% goto upx
+goto compress
+
+REM Running UPX to compress executables (if set...)
+:upx
+"%UPX_BIN%" -9 Converter\bin\convert.exe
+"%UPX_BIN%" -9 Editor\bin\sfqsubed.exe
+goto compress
+
+REM making the release
+:compress
+set ARCHIVE_PATH=..\%RELEASE_BASE_DIR%
+set ARCHIVE_NAME=%ARCHIVE_PATH%\sfqsubed_%VERSION%.zip
+
+REM Compressing the Converter program
+cd Converter\bin\
+"%SEVENZIP_BIN%" a "..\..\%ARCHIVE_PATH%\convert.zip" batconv.cmd convert.exe tutorial.txt
+cd ..\..\ 
+
+REM Compressing the Free Quest program
+cd Editor\bin\
+"%SEVENZIP_BIN%" a -r -x!.* "..\..\%ARCHIVE_NAME%" "..\..\%ARCHIVE_PATH%\convert.zip" "docs\*.*" "data\*.*" sfqsubed.exe
+cd ..\..\
+if exist "%ARCHIVE_PATH%\convert.zip" del "%ARCHIVE_PATH%\convert.zip"
+
+REM Checking...
+if not exist "%ARCHIVE_NAME%" goto mk_error
+goto mk_success
+
+:mk_error
+echo ERROR: The archive "%ARCHIVE_NAME%" wasn't created...
+pause
+goto end
+
+:mk_success
+echo SUCCESS: The archive "%ARCHIVE_NAME%" was created.
+goto end
+
+REM This is the end
+:end
