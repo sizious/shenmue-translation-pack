db9497022060027656222549d1fdb0795fb66994
sizious <sizious@e94a5bfb-cbc5-4b52-91e4-5eccdaea5047>
2009-09-04 21:32:29 +0000

    Updating FQ editor, AFS Utils, Common directory (with tools)

--- AiO Free Quest Subtitles Editor/Editor/src/engine/multitrd/ui/iconsui.pas
+++ AiO Free Quest Subtitles Editor/Editor/src/engine/multitrd/ui/iconsui.pas
@@ -2,6 +2,21 @@ unit iconsui;
 
 interface
 
+{ GT = Global-Translation
+  This file is here to identify each Image from the ImageList associated to
+  the Global-Translation view. }
+
+const
+  GT_ICON_NOT_TRANSLATED            = 0;
+  GT_ICON_TRANSLATED                = 1;
+  GT_ICON_TEXT_MODIFIED             = 2;
+  GT_ICON_SUBTITLES_FOLDER          = 3;
+  GT_ICON_PAKS_FILE                 = 4;
+  GT_ICON_SUBTITLE_CODE             = 5;
+  GT_ICON_TRANSLATED_TEXT           = 6;
+  GT_ICON_TRANSLATION_IN_PROGRESS   = 7;
+  GT_ICON_ERRORNOUS_SUBTITLE        = 8;
+  
 implementation
 
 end.
