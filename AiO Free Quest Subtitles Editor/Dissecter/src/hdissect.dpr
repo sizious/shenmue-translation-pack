a9f5e22a076231e67d662472d6b62015577bbaf8
SiZiOUS <15776845+sizious@users.noreply.github.com>
2010-02-03 14:06:47 +0000

    update HUMANS Dissecter 1.3

--- AiO Free Quest Subtitles Editor/Dissecter/src/hdissect.dpr
+++ AiO Free Quest Subtitles Editor/Dissecter/src/hdissect.dpr
@@ -3,7 +3,13 @@ program hdissect;
 uses
   Forms,
   main in 'main.pas' {Form1},
-  config in 'config.pas';
+  config in 'config.pas',
+  scnfedit in '..\..\Editor\src\engine\scnfedit.pas',
+  common in '..\..\Editor\src\engine\common.pas',
+  scnfutil in '..\..\Editor\src\engine\scnfutil.pas',
+  charslst in '..\..\Editor\src\engine\charslst.pas',
+  charscnt in '..\..\Editor\src\engine\charscnt.pas',
+  npcinfo in '..\..\Editor\src\engine\npcinfo.pas';
 
 {$R *.res}
 
