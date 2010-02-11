44815e9f882cf9206faf0ed7f38740e05e20c004
SiZiOUS <15776845+sizious@users.noreply.github.com>
2010-02-11 20:21:46 +0000

    Commiting IPAC browser...

--- IPAC Browser/src/engine/ipacmgr.pas
+++ IPAC Browser/src/engine/ipacmgr.pas
@@ -14,14 +14,16 @@ type
   TFileSectionsListItem = class
   private
     fName: string;
-    fSize: Integer;
-    fAbsoluteOffset: Integer;
+    fSize: LongWord;
+    fAbsoluteOffset: LongWord;
     fOwner: TFileSectionsList;
+    fIndex: Integer;
   public
     constructor Create(Owner: TFileSectionsList);
+    property Index: Integer read fIndex;
     property Name: string read fName;
-    property Offset: Integer read fAbsoluteOffset;
-    property Size: Integer read fSize;
+    property Offset: LongWord read fAbsoluteOffset;
+    property Size: LongWord read fSize;
     property Owner: TFileSectionsList read fOwner;
   end;
 
@@ -33,7 +35,7 @@ type
     function GetCount: Integer;
     function GetItem(Index: Integer): TFileSectionsListItem;
   protected
-    function Add(Name: string; AbsoluteOffset, Size: Integer): Integer;
+    function Add(Name: string; AbsoluteOffset, Size: LongWord): Integer;
     procedure Clear;
   public
     constructor Create(Owner: TIpacEditor);
@@ -49,50 +51,65 @@ type
     fOwner: TIpacSectionsList;
     fName: string;
     fKind: string;
-    fSize: Integer;
-    fRelativeOffset: Integer;
-    fAbsoluteOffset: Integer;
+    fSize: LongWord;
+    fRelativeOffset: LongWord;
+    fAbsoluteOffset: LongWord;
     fImportedFileName: TFileName;
-    fIsImported: Boolean;
+    fUpdated: Boolean;
+    fIndex: Integer;
+    function GetSourceFileName: TFileName;
+    function GetFileExtension: string;
   protected
-    procedure WriteIpacSection(var FS: TFileStream);
+    function GetSectionPaddingSize: LongWord;
+    procedure WriteIpacSection(var InStream, OutStream: TFileStream);
+    function KindToExtension(Kind: string): string;
+    property SourceFileName: TFileName read GetSourceFileName;
   public
     constructor Create(Owner: TIpacSectionsList);
 
     procedure CancelImport;
     procedure ExportToFile(const FileName: TFileName);
-    function ExportToFolder(const Folder: TFileName): TFileName; overload;
+    function ExportToFolder(const Folder: TFileName): TFileName;
     function GetOutputFileName: TFileName;
-    function ImportFromFile(const FileName: TFileName): Boolean;    
+    function ImportFromFile(const FileName: TFileName): Boolean;
 
-    property AbsoluteOffset: Integer read fAbsoluteOffset;
+    property AbsoluteOffset: LongWord read fAbsoluteOffset;
+    property FileExtension: string read GetFileExtension;
+    property Index: Integer read fIndex;    
     property ImportedFileName: TFileName read fImportedFileName;
-    property IsImported: Boolean read fIsImported;
     property Name: string read fName;
     property Kind: string read fKind;
-    property RelativeOffset: Integer read fRelativeOffset;
-    property Size: Integer read fSize;
+    property PaddingSize: LongWord read GetSectionPaddingSize;
+    property RelativeOffset: LongWord read fRelativeOffset;
+    property Size: LongWord read fSize;
     property Owner: TIpacSectionsList read fOwner;
+    property Updated: Boolean read fUpdated;    
   end;
 
   // IPAC sections manager
   TIpacSectionsList = class
   private
+    fIpacSectionSizeAbsoluteOffset1: LongWord;
+    fIpacSectionSizeAbsoluteOffset2: LongWord;
     fOwner: TIpacEditor;
     fList: TList;
+    fIpacFileSectionIndex: Integer;
     function GetCount: Integer;
     function GetItem(Index: Integer): TIpacSectionsListItem;
+    function GetIpacFileSection: TFileSectionsListItem;
   protected
     function Add(Name, Kind: string; AbsoluteOffset, RelativeOffset,
-      Size: Integer): Integer;
+      Size: LongWord): Integer;
     procedure Clear;
+    procedure WriteIpacHeader(var FS: TFileStream);
     procedure WriteIpacFooter(var FS: TFileStream);
   public
     constructor Create(Owner: TIpacEditor);
     destructor Destroy; override;
     property Count: Integer read GetCount;
     property Items[Index: Integer]: TIpacSectionsListItem read GetItem; default;
-    property Owner: TIpacEditor read fOwner;    
+    property IpacSection: TFileSectionsListItem read GetIpacFileSection;
+    property Owner: TIpacEditor read fOwner;
   end;
 
   // Main class
@@ -103,7 +120,7 @@ type
     fFileSectionsList: TFileSectionsList;
     fSourceFileName: TFileName;
   protected
-    procedure ParseIpacSections(var FS: TFileStream; IpacOffset, IpacSize: Integer);
+    procedure ParseIpacSections(var FS: TFileStream; IpacOffset, IpacSize: LongWord);
   public
     constructor Create;
     destructor Destroy; override;
@@ -127,30 +144,39 @@ type
 implementation
 
 uses
-  SysTools;
+  SysTools, IpacUtil;
   
 type
   TSectionEntry = record
     Name: array[0..3] of Char;
-    Size: Integer;
+    Size: LongWord;
   end;
 
   TIpacSectionEntry = record
     Name: array[0..7] of Char;
     Kind: array[0..3] of Char; // type of the footer entry ('BIN ', 'CHRM...')
-    Offset: Integer;
-    Size: Integer;
+    Offset: LongWord;
+    Size: LongWord;
+  end;
+
+  TIpacSectionKind = record
+    Name: string;
+    Extension: string;
   end;
 
 const
   IPAC_SIGN = 'IPAC';
-  
+  IPAC_SECTION_KINDS: array[0..1] of TIpacSectionKind = (
+    (Name: IPAC_BIN ; Extension: 'BIN'),
+    (Name: IPAC_CHRM; Extension: 'CHR')
+  );
+
 { TIpacSectionsListItem }
 
 procedure TIpacSectionsListItem.CancelImport;
 begin
   fImportedFileName := '';
-  fIsImported := False;
+  fUpdated := False;
 end;
 
 constructor TIpacSectionsListItem.Create(Owner: TIpacSectionsList);
@@ -159,59 +185,159 @@ begin
 end;
 
 procedure TIpacSectionsListItem.ExportToFile(const FileName: TFileName);
+var
+  InStream, OutStream: TFileStream;
+
 begin
-  
+  OutStream := TFileStream.Create(FileName, fmCreate);
+  InStream := TFileStream.Create(GetSourceFileName, fmOpenRead);
+  try
+    InStream.Seek(AbsoluteOffset, soFromBeginning);
+    OutStream.CopyFrom(InStream, Size);
+  finally
+    OutStream.Free;
+    InStream.Free;
+  end;
 end;
 
-function TIpacSectionsListItem.ExportToFolder(
-  const Folder: TFileName): TFileName;
+function TIpacSectionsListItem.ExportToFolder(const Folder: TFileName): TFileName;
 begin
+  Result := IncludeTrailingPathDelimiter(Folder) + GetOutputFileName;
+  ExportToFile(Result);
+end;
 
+function TIpacSectionsListItem.GetFileExtension: string;
+begin
+  Result := '.' + KindToExtension(Kind);
 end;
 
 function TIpacSectionsListItem.GetOutputFileName: TFileName;
-begin
+(*var
+  RadicalName: TFileName;
 
+begin
+  RadicalName := ChangeFileExt(ExtractFileName(Owner.Owner.SourceFileName));
+  Result := RadicalName + '_' + Name + '_#' + IntToStr(Index) + '.BIN';*)
+begin
+  Result := Name + '.' + KindToExtension(Kind);
 end;
 
 function TIpacSectionsListItem.ImportFromFile(
   const FileName: TFileName): Boolean;
 begin
+  Result := FileExists(FileName);
+  if not Result then Exit;
+
   fImportedFileName := FileName;
-  fIsImported := True;
+  fUpdated := True;
 end;
 
-procedure TIpacSectionsListItem.WriteIpacSection(var FS: TFileStream);
-begin
+function TIpacSectionsListItem.KindToExtension(Kind: string): string;
+var
+  i: Integer;
 
+begin
+  for i := Low(IPAC_SECTION_KINDS) to High(IPAC_SECTION_KINDS) do
+    if IPAC_SECTION_KINDS[i].Name = Kind then begin
+      Result := IPAC_SECTION_KINDS[i].Extension;
+    end;
+end;
+
+procedure TIpacSectionsListItem.WriteIpacSection(var InStream, OutStream: TFileStream);
+var
+  Offset, IpacAbsoluteOffset,
+  NewSectionSize: LongWord;
+  ImportedFileStream: TFileStream;
+  Buffer: array[0..31] of Char;
+
+begin
+  Offset := OutStream.Position;
+  IpacAbsoluteOffset := Owner.IpacSection.Offset;
+  
+  if Updated then begin
+
+    // Importing the new section
+    ImportedFileStream := TFileStream.Create(ImportedFileName, fmOpenRead);
+    try
+      NewSectionSize := ImportedFileStream.Size;
+      OutStream.CopyFrom(ImportedFileStream, NewSectionSize);
+    finally
+      ImportedFileStream.Free;
+    end;  
+
+  end else begin
+    // Raw Copy of the section
+    NewSectionSize := Size;
+    InStream.Seek(AbsoluteOffset, soFromBeginning);
+    OutStream.CopyFrom(InStream, NewSectionSize);
+  end;
+
+  // Updating footer infos
+  Owner.Items[Index].fAbsoluteOffset := Offset;
+  Owner.Items[Index].fRelativeOffset := Offset - IpacAbsoluteOffset;
+  Owner.Items[Index].fSize := NewSectionSize; // important because PaddingSize is set after!
+
+  // Writing section padding
+  ZeroMemory(@Buffer, PaddingSize);
+  OutStream.Write(Buffer, PaddingSize);
+end;
+
+function TIpacSectionsListItem.GetSectionPaddingSize: LongWord;
+var
+  current_num, total_null_bytes: LongWord;
+
+begin
+  // Finding the correct number of null bytes after file data
+  // Based on the original function by Manic
+  current_num := 0;
+  total_null_bytes := 0;
+  while current_num <> Size do
+  begin
+    if total_null_bytes = 0 then begin
+      total_null_bytes := 31;
+    end
+    else begin
+      Dec(total_null_bytes);
+    end;
+    Inc(current_num);
+  end;
+  Result := total_null_bytes;
+end;
+
+function TIpacSectionsListItem.GetSourceFileName: TFileName;
+begin
+  Result := Owner.Owner.SourceFileName;
 end;
 
 { TIPACEditor }
 
-procedure TIPACEditor.Clear;
+procedure TIpacEditor.Clear;
 begin
-  fSections.Clear;
+  Sections.Clear;
+  Content.Clear;
 end;
 
-constructor TIPACEditor.Create;
+constructor TIpacEditor.Create;
 begin
   fSections := TIpacSectionsList.Create(Self);
   fFileSectionsList := TFileSectionsList.Create(Self);
 end;
 
-destructor TIPACEditor.Destroy;
+destructor TIpacEditor.Destroy;
 begin
   fSections.Free;
   fFileSectionsList.Free;
   inherited;
 end;
 
-function TIPACEditor.LoadFromFile(const FileName: TFileName): Boolean;
+function TIpacEditor.LoadFromFile(const FileName: TFileName): Boolean;
 var
   FS: TFileStream;
   SectionEntry: TSectionEntry;
-  Offset: Integer;
-  
+  Offset, NextSectionOffset: LongWord;
+  IpacSectionIndex: Integer;
+  Done: Boolean;
+
 begin
   Result := False;
   if not FileExists(FileName) then Exit;
@@ -223,7 +349,7 @@ begin
   try
 
 {$IFDEF DEBUG}
-    WriteLn('*** PARSING FILE ***');
+    WriteLn(sLineBreak, '*** PARSING FILE ***');
 {$ENDIF}
 
     // Parsing the file...
@@ -232,29 +358,43 @@ begin
 
       // Reading the header
       FS.Read(SectionEntry, SizeOf(TSectionEntry));
-      Sections.Add(SectionEntry.Name, Offset, SectionEntry.Size);
+      IpacSectionIndex := Sections.Add(SectionEntry.Name, Offset, SectionEntry.Size);
+      NextSectionOffset := Offset + SectionEntry.Size;
 
       // We found the IPAC section
       if SectionEntry.Name = IPAC_SIGN then begin
         Result := True;
-        ParseIpacSections(FS, Offset, SectionEntry.Size); // parse the IPAC section
+        Content.fIpacFileSectionIndex := IpacSectionIndex;
+
+        // Parse the IPAC section
+        with Content.IpacSection do
+          ParseIpacSections(FS, Offset, Size);
+
+        // Skip IPAC footer
+        Inc(NextSectionOffset, Content.Count * SizeOf(TIpacSectionEntry));
       end;
 
       // Skipping section
-      FS.Seek(Offset + SectionEntry.Size, soFromBeginning);
+      Done := (NextSectionOffset >= FS.Size) or (SectionEntry.Size = 0);
+      if not Result then
+        FS.Seek(NextSectionOffset, soFromBeginning);
       
-    until Result or (FS.Position >= FS.Size);
+    until Done or (FS.Position >= FS.Size);
 
     // Not a valid file...
     if not Result then
       Clear;
+
+{$IFDEF DEBUG}
+    WriteLn(sLineBreak, 'LoadFromFile "', ExtractFileName(FileName), '" result: ', Result);
+{$ENDIF}
 
   finally
     FS.Free;
   end;
 end;
 
-procedure TIPACEditor.ParseIpacSections(var FS: TFileStream; IpacOffset, IpacSize: Integer);
+procedure TIpacEditor.ParseIpacSections(var FS: TFileStream; IpacOffset, IpacSize: LongWord);
 var
   IpacSectionsCount: Longword;
   IpacSection: TIpacSectionEntry;
@@ -271,7 +411,7 @@ begin
 
   // Reading every IPAC entries in the footer
 {$IFDEF DEBUG}
-  WriteLn(sLineBreak, '*** READING IPAC SECTION ***');
+  WriteLn(sLineBreak, 'Reading IPAC sections:');
 {$ENDIF}
 
   for i := 0 to IpacSectionsCount - 1 do begin
@@ -285,33 +425,39 @@ begin
 {$ENDIF}
 end;
 
-function TIPACEditor.Save: Boolean;
+function TIpacEditor.Save: Boolean;
 begin
   Result := SaveToFile(SourceFileName);
 end;
 
-function TIPACEditor.SaveToFile(const FileName: TFileName): Boolean;
+function TIpacEditor.SaveToFile(const FileName: TFileName): Boolean;
 var
   InStream, OutStream: TFileStream;
-  OutFile: TFileName;
-  i: Integer;
+  OutTempFile: TFileName;
+  i, j: Integer;
   
 begin
-  Result := False;
-
-  OutFile := GetTempFileName;
+  OutTempFile := GetTempFileName;
 
   // Opening the file
-  OutStream := TFileStream.Create(FileName, fmCreate);
+  OutStream := TFileStream.Create(OutTempFile, fmCreate);
   InStream := TFileStream.Create(SourceFileName, fmOpenRead);
   try
+
+{$IFDEF DEBUG}
+    WriteLn('*** SAVING THE FILE ***');
+{$ENDIF}
 
     // Writing each file section
     for i := 0 to Sections.Count - 1 do begin
 
       if Sections[i].Name = IPAC_SIGN then begin
-        (*InStream.Seek(Sections[i].Offset, soFromBeginning);
-        OutStream.CopyFrom(InStream, Sections[i].Size);*)
+        // Write IPAC header
+        Content.WriteIpacHeader(OutStream);
+
+        // Write IPAC sections content
+        for j := 0 to Content.Count - 1 do
+          Content[j].WriteIpacSection(InStream, OutStream);
 
         // Writing IPAC footer
         Content.WriteIpacFooter(OutStream);
@@ -324,6 +470,11 @@ begin
 
     end;
 
+  finally
+    // Destroying FileStream
+    OutStream.Free;
+    InStream.Free;
+
     // Checking target
     if FileExists(FileName) then
       if (MakeBackup) and (FileName = SourceFileName) then
@@ -332,18 +483,27 @@ begin
         DeleteFile(FileName);
 
     // Copying the right file
-    Result := CopyFile(OutFile, FileName, True);
+    Result := CopyFile(OutTempFile, FileName, False);
 
-  finally
-    OutStream.Free;
-    InStream.Free;
+    // Deleting temp file
+    if Result then
+      DeleteFile(OutTempFile);
+
+    (*  Reloading the file from the disk because every structure must be reloaded
+        cleared datas. Every infos has been modified in order to build the output
+        file. It's crap, but doing so uses less variables, so less source code. *)
+    Result := Result and LoadFromFile(SourceFileName);
+
+{$IFDEF DEBUG}
+    WriteLn('Saved to "', ExtractFileName(FileName), '", Result: ', Result);
+{$ENDIF}
   end;
 end;
 
 { TIpacSectionsList }
 
 function TIpacSectionsList.Add(Name, Kind: string; AbsoluteOffset, RelativeOffset,
-  Size: Integer): Integer;
+  Size: LongWord): Integer;
 var
   NewItem: TIpacSectionsListItem;
 
@@ -355,6 +515,7 @@ begin
   NewItem.fRelativeOffset := RelativeOffset;
   NewItem.fSize := Size;
   Result := fList.Add(NewItem);
+  NewItem.fIndex := Result;
 
 {$IFDEF DEBUG}
   WriteLn('  #', Result, ': Name: ', Name, ', Kind: ', Kind,
@@ -373,7 +534,7 @@ begin
   fList.Clear;
 end;
 
-constructor TIpacSectionsList.Create(Owner: TIPACEditor);
+constructor TIpacSectionsList.Create(Owner: TIpacEditor);
 begin
   fOwner := Owner;
   fList := TList.Create;
@@ -391,17 +552,29 @@ begin
   Result := fList.Count;
 end;
 
+function TIpacSectionsList.GetIpacFileSection: TFileSectionsListItem;
+begin
+  Result := Owner.Sections[fIpacFileSectionIndex];
+end;
+
 function TIpacSectionsList.GetItem(Index: Integer): TIpacSectionsListItem;
 begin
   Result := TIpacSectionsListItem(fList.Items[Index]);
 end;
 
 procedure TIpacSectionsList.WriteIpacFooter(var FS: TFileStream);
+const
+  IPAC_HEADER_SIZE = 16;
+
 var
   i: Integer;
   Entry: TIpacSectionEntry;
+  Offset: LongWord;
 
 begin
+  IpacSection.fSize := IPAC_HEADER_SIZE;
+  
+  // Writing each footer entry
   for i := 0 to Count - 1 do begin
     // Preparing the Entry raw string fields
     ZeroMemory(@Entry.Name, Length(Entry.Name));
@@ -415,12 +588,62 @@ begin
 
     // Writing to the file
     FS.Write(Entry, SizeOf(TIpacSectionEntry));
+
+    // Calculating Ipac Section size
+    Inc(IpacSection.fSize, Items[i].Size);
+    Inc(IpacSection.fSize, Items[i].PaddingSize);
+
+{$IFDEF DEBUG}
+    with Items[i] do
+      WriteLn('  #', i, ': Name: ', Name, ', Kind: ', Kind, ', ROffset: ',
+        RelativeOffset, ', AOffset: ', AbsoluteOffset, ', Size: ', Size);
+{$ENDIF}
   end;
+
+  // saving the end of flow offset
+  Offset := FS.Position;
+
+  // Updating IPAC header
+  FS.Seek(fIpacSectionSizeAbsoluteOffset1, soFromBeginning);
+  FS.Write(IpacSection.Size, SizeOf(LongWord));
+  FS.Seek(fIpacSectionSizeAbsoluteOffset2, soFromBeginning);
+  FS.Write(IpacSection.Size, SizeOf(LongWord));
+
+{$IFDEF DEBUG}
+  WriteLn(sLineBreak, 'IPAC section size: ', IpacSection.Size, sLineBreak);
+{$ENDIF}
+
+  // Restoring the offset
+  FS.Seek(Offset, soFromBeginning);
+end;
+
+procedure TIpacSectionsList.WriteIpacHeader(var FS: TFileStream);
+const
+  IPAC_FAKE_SIZE: LongWord = $FFFFFFFF;
+
+var
+  SectionsCount: LongWord;
+
+begin
+  // Write signature
+  FS.Write(IPAC_SIGN, Length(IPAC_SIGN));
+
+  // Write IPAC size (to update after)
+  fIpacSectionSizeAbsoluteOffset1 := FS.Position;
+  FS.Write(IPAC_FAKE_SIZE, SizeOf(IPAC_FAKE_SIZE));
+
+  // Write IPAC sections count
+  SectionsCount := LongWord(Count);
+  FS.Write(SectionsCount, SizeOf(LongWord));
+
+  // Write IPAC size (to update after again)
+  fIpacSectionSizeAbsoluteOffset2 := FS.Position;
+  FS.Write(IPAC_FAKE_SIZE, SizeOf(IPAC_FAKE_SIZE));  
 end;
 
 { TFileSectionsList }
 
-function TFileSectionsList.Add(Name: string; AbsoluteOffset, Size: Integer): Integer;
+function TFileSectionsList.Add(Name: string; AbsoluteOffset, Size: LongWord): Integer;
 var
   Item: TFileSectionsListItem;
 
@@ -430,6 +653,7 @@ begin
   Item.fAbsoluteOffset := AbsoluteOffset;
   Item.fSize := Size;
   Result := fList.Add(Item);
+  Item.fIndex := Result;
 
 {$IFDEF DEBUG}
   WriteLn('  #', Result, ': Name: ', Name, ', AbsoluteOffset: ',
@@ -478,3 +702,4 @@ begin
 end;
 
 end.
+
--- IPAC Browser/src/main.pas
+++ IPAC Browser/src/main.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, IpacMgr, Menus;
+  Dialogs, IpacMgr, Menus, ComCtrls, JvExComCtrls, JvListView, ImgList, ToolWin,
+  JvToolBar, Themes;
 
 type
   TfrmMain = class(TForm)
@@ -14,41 +15,193 @@ type
     N1: TMenuItem;
     miQuit: TMenuItem;
     miDebugMenu: TMenuItem;
-    estIPACEditor1: TMenuItem;
+    miDebugTest1: TMenuItem;
+    lvIpacContent: TJvListView;
+    StatusBar1: TStatusBar;
+    od: TOpenDialog;
+    ilIpacContent: TImageList;
+    ilHeader: TImageList;
+    JvToolBar1: TJvToolBar;
+    ToolButton1: TToolButton;
+    tbOpen: TToolButton;
+    Help1: TMenuItem;
+    About1: TMenuItem;
+    Close1: TMenuItem;
+    Save1: TMenuItem;
+    Saveas1: TMenuItem;
+    N2: TMenuItem;
+    N3: TMenuItem;
+    Reload1: TMenuItem;
+    Edit1: TMenuItem;
+    Undochanges1: TMenuItem;
+    N4: TMenuItem;
+    Import1: TMenuItem;
+    Export1: TMenuItem;
+    N5: TMenuItem;
+    Exportall1: TMenuItem;
+    View1: TMenuItem;
+    Debug1: TMenuItem;
+    Fileinformations1: TMenuItem;
+    N6: TMenuItem;
+    ilToolBar: TImageList;
+    ToolButton2: TToolButton;
+    ToolButton3: TToolButton;
+    ToolButton4: TToolButton;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
-    procedure estIPACEditor1Click(Sender: TObject);
+    procedure miDebugTest1Click(Sender: TObject);
+    procedure miOpenClick(Sender: TObject);
+    procedure lvIpacContentColumnClick(Sender: TObject; Column: TListColumn);
+    procedure JvToolBar1CustomDraw(Sender: TToolBar; const ARect: TRect;
+      var DefaultDraw: Boolean);
+    procedure miQuitClick(Sender: TObject);
   private
     { Déclarations privées }
+    procedure Clear;
+    procedure ClearColumnsImages;
+    function KindToImageIndex(Kind: string): Integer;
+    procedure LoadFile(const FileName: TFileName);
   public
     { Déclarations publiques }
+    function MsgBox(Text, Caption: string; Flags: Integer): Integer;
   end;
 
 var
   frmMain: TfrmMain;
-  IPACEditor: TIPACEditor;
+  IPACEditor: TIpacEditor;
 
 implementation
 
 {$R *.dfm}
 
-procedure TfrmMain.estIPACEditor1Click(Sender: TObject);
+uses
+  IpacUtil;
+
+procedure TfrmMain.Clear;
 begin
-  IPACEditor.LoadFromFile('afs00001.PKS');
+  ClearColumnsImages;
+  lvIpacContent.Clear;
+end;
+
+procedure TfrmMain.ClearColumnsImages;
+var
+  i: Integer;
+  
+begin
+  for i := 0 to lvIpacContent.Columns.Count - 1 do
+    lvIpacContent.Column[i].ImageIndex := -1;
+end;
+
+procedure TfrmMain.miDebugTest1Click(Sender: TObject);
+begin
+  IPACEditor.LoadFromFile('AKMI.PKS');
+  IPACEditor.Content[0].ExportToFile('0_before.bin');
   IPACEditor.Content[0].ImportFromFile('TEST.BIN');
+  ipaceditor.Content[1].ImportFromFile('scnf.bin');
+  ipaceditor.Content[1].ExportToFolder('.\');
+  ipaceditor.Content[1].CancelImport;
   IPACEditor.SaveToFile('OUT.BIN');
+//  ipaceditor.Save;
+  IPACEditor.Content[0].ExportToFile('0_after.bin');
 end;
 
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
+  Caption := Application.Title;
+
   // Creating the main IPAC Editor object
-  IPACEditor := TIPACEditor.Create;
+  IPACEditor := TIpacEditor.Create;
+
+  Constraints.MinHeight := Height;
+  Constraints.MinWidth := Width;
 end;
 
 procedure TfrmMain.FormDestroy(Sender: TObject);
 begin
   // Destroying the IPAC Object
   IPACEditor.Free;
+end;
+
+procedure TfrmMain.JvToolBar1CustomDraw(Sender: TToolBar; const ARect: TRect;
+  var DefaultDraw: Boolean);
+var
+  ElementDetails: TThemedElementDetails;
+  NewRect : TRect;
+
+begin
+  // Thank you ...
+  // http://www.brandonstaggs.com/2009/06/29/give-a-delphi-ttoolbar-a-proper-themed-background/
+  if ThemeServices.ThemesEnabled then begin
+    NewRect := Sender.ClientRect;
+    NewRect.Top := NewRect.Top - GetSystemMetrics(SM_CYMENU);
+    ElementDetails := ThemeServices.GetElementDetails(trRebarRoot);
+    ThemeServices.DrawElement(Sender.Canvas.Handle, ElementDetails, NewRect);
+  end;
+end;
+
+function TfrmMain.KindToImageIndex(Kind: string): Integer;
+begin
+  Result := -1;
+  if Kind = IPAC_BIN then
+    Result := 0
+  else if Kind = IPAC_CHRM then
+    Result := 1;
+end;
+
+procedure TfrmMain.LoadFile(const FileName: TFileName);
+var
+  i: Integer;
+
+begin
+  if IPACEditor.LoadFromFile(FileName) then begin
+    Clear;
+
+    // Adding entries
+    for i := 0 to IPACEditor.Content.Count - 1 do
+      with lvIpacContent.Items.Add do
+        with IPACEditor.Content[i] do begin
+          Caption := Name + FileExtension;
+          SubItems.Add(KindToFriendlyName(Kind));
+          SubItems.Add(IntToStr(AbsoluteOffset));
+          SubItems.Add(IntToStr(Size));
+          ImageIndex := KindToImageIndex(Kind);
+        end;
+
+  end else
+    MsgBox('This file doesn''t contain a valid IPAC section.', 'Warning', MB_ICONWARNING);
+end;
+
+procedure TfrmMain.lvIpacContentColumnClick(Sender: TObject;
+  Column: TListColumn);
+var
+  OldIndex: Integer;
+
+begin
+  OldIndex := Column.ImageIndex;
+  
+  ClearColumnsImages;
+
+  if OldIndex = -1 then
+    Column.ImageIndex := 0
+  else
+    Column.ImageIndex := (OldIndex + 1) mod ilHeader.Count;
+end;
+
+procedure TfrmMain.miOpenClick(Sender: TObject);
+begin
+  with od do
+    if Execute then
+      LoadFile(FileName);
+end;
+
+procedure TfrmMain.miQuitClick(Sender: TObject);
+begin
+  Close;
+end;
+
+function TfrmMain.MsgBox(Text, Caption: string; Flags: Integer): Integer;
+begin
+  Result := MessageBoxA(Handle, PChar(Text), PChar(Caption), Flags);
 end;
 
 end.
