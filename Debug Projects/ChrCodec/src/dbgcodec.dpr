0883f81cb7c468bcf206e4a0b965cee50cc06d4f
SiZiOUS <15776845+sizious@users.noreply.github.com>
2010-07-10 15:45:14 +0000

    Debug projects updated

--- Debug Projects/ChrCodec/src/dbgcodec.dpr
+++ Debug Projects/ChrCodec/src/dbgcodec.dpr
@@ -8,11 +8,22 @@ uses
   chrcodec in '..\..\..\Common\SubsUtil\chrcodec.pas',
   systools in '..\..\..\Common\systools.pas';
 
+var
+  Codec: TShenmueCharsetCodec;
+
 begin
+  Codec := TShenmueCharsetCodec.Create;
   try
-  { TODO -oUtilisateur -cCode du point d'entrée : Placez le code ici }
-  except
-    on E:Exception do
-      Writeln(E.Classname, ': ', E.Message);
+    try
+      Codec.LoadFromFile('chrlist1.csv');
+      WriteLn(Codec.Encode('Sélécti...éonnéz votre jeù.....o''l' + #13#10 + 'dirty bastààrd..'));
+      WriteLn(Codec.Decode('c''m©nÆ Æ bÄbÎ=@ it''s timÆ..' + #$81#$95 + 'tÌ gÆt mÌnÆy.'));
+      ReadLn;
+    except
+      on E:Exception do
+        Writeln(E.Classname, ': ', E.Message);
+    end;
+  finally
+    Codec.Free;
   end;
 end.
