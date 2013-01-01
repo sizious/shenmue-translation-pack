9d1bf19d50041055ee6587cb48a12ac095b4beda
SiZiOUS <15776845+sizious@users.noreply.github.com>
2013-01-01 16:18:16 +0000

    project refactoring

--- Free Quest Subtitles Editor/clean.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-if exist "Converter\bin\*.exe" del "Converter\bin\*.exe"
-if exist "Converter\obj\*.dcu" del "Converter\obj\*.dcu"
-if exist "Editor\bin\*.exe" del "Editor\bin\*.exe"
-if exist "Editor\obj\*.dcu" del "Editor\obj\*.dcu"
