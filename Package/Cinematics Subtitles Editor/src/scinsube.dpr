8d6865f858561a9b90b8563ef5acbb997dbfc0d7
sizious <sizious@e94a5bfb-cbc5-4b52-91e4-5eccdaea5047>
2013-08-28 14:19:24 +0000

    Fixing some 'Common' source directory references

--- Package/Cinematics Subtitles Editor/src/scinsube.dpr
+++ Package/Cinematics Subtitles Editor/src/scinsube.dpr
@@ -14,7 +14,32 @@ uses
   subsimp in 'engine\subsimp.pas',
   warning in 'warning.pas' {frmWarning},
   utextdb in 'engine\utextdb.pas',
-  srfkeydb in 'engine\srfkeydb.pas';
+  srfkeydb in 'engine\srfkeydb.pas',
+  fileslst in '..\..\..\Common\fileslst.pas',
+  hashidx in '..\..\..\Common\hashidx.pas',
+  chrcodec in '..\..\..\Common\SubsUtil\chrcodec.pas',
+  systools in '..\..\..\Common\systools.pas',
+  filespec in '..\..\..\Common\filespec.pas',
+  lzmadec in '..\..\..\Common\lzmadec.pas',
+  workdir in '..\..\..\Common\workdir.pas',
+  MD5Api in '..\..\..\Common\MD5\MD5Api.pas',
+  MD5Core in '..\..\..\Common\MD5\MD5Core.pas',
+  oldskool_font_mapper in '..\..\..\Common\SubsUtil\Preview\oldskool_font_mapper.pas',
+  oldskool_font_vcl in '..\..\..\Common\SubsUtil\Preview\oldskool_font_vcl.pas',
+  viewer in '..\..\..\Common\SubsUtil\Preview\viewer.pas' {frmSubsPreview},
+  bugsmgr in '..\..\..\Common\BugsMan\bugsmgr.pas' {frmBugsHandler},
+  debuglog in '..\..\..\Common\DebugLog\debuglog.pas' {frmDebugLog},
+  xmlconf in '..\..\..\Common\xmlconf.pas',
+  dbindex in '..\..\..\Common\TextDB\dbindex.pas',
+  dbinlay in '..\..\..\Common\TextDB\dbinlay.pas',
+  textdb in '..\..\..\Common\TextDB\textdb.pas',
+  appver in '..\..\..\Common\appver.pas',
+  batchexe in '..\..\..\Common\batchexe.pas',
+  uitools in '..\..\..\Common\uitools.pas',
+  progress in '..\..\..\Common\Progress\progress.pas' {ProgressWindow},
+  dirscan in '..\..\..\Common\DirScan\dirscan.pas' {DirectoryScannerQueryWindow},
+  about in '..\..\..\Common\About\about.pas' {frmAbout},
+  chrcount in '..\..\..\Common\SubsUtil\chrcount.pas';
 
 {$R *.res}
 
--- Package/Special Scenes Subtitles Editor/src/spcscned.dpr
+++ Package/Special Scenes Subtitles Editor/src/spcscned.dpr
@@ -9,7 +9,24 @@ uses
   main in 'main.pas' {frmMain},
   seqedit in 'engine\seqedit.pas',
   config in 'config.pas',
-  seqdb in 'engine\seqdb.pas';
+  seqdb in 'engine\seqdb.pas',
+  oldskool_font_mapper in '..\..\..\Common\SubsUtil\Preview\oldskool_font_mapper.pas',
+  oldskool_font_vcl in '..\..\..\Common\SubsUtil\Preview\oldskool_font_vcl.pas',
+  viewer in '..\..\..\Common\SubsUtil\Preview\viewer.pas' {frmSubsPreview},
+  bugsmgr in '..\..\..\Common\BugsMan\bugsmgr.pas' {frmBugsHandler},
+  debuglog in '..\..\..\Common\DebugLog\debuglog.pas' {frmDebugLog},
+  xmlconf in '..\..\..\Common\xmlconf.pas',
+  chrcodec in '..\..\..\Common\SubsUtil\chrcodec.pas',
+  chrcount in '..\..\..\Common\SubsUtil\chrcount.pas',
+  hashidx in '..\..\..\Common\hashidx.pas',
+  systools in '..\..\..\Common\systools.pas',
+  fsparser in '..\..\..\Common\fsparser.pas',
+  filespec in '..\..\..\Common\filespec.pas',
+  workdir in '..\..\..\Common\workdir.pas',
+  lzmadec in '..\..\..\Common\lzmadec.pas',
+  appver in '..\..\..\Common\appver.pas',
+  uitools in '..\..\..\Common\uitools.pas',
+  about in '..\..\..\Common\About\about.pas' {frmAbout};
 
 {$R *.res}
 
